384c6f4192d8f954c5641ffaede136acd9f85c8d
Yuting-H <yuting.yh@gmail.com>
2023-07-11 19:22:56 -0400

    added string values

--- BirthdayCard/MainActivity.kt
+++ BirthdayCard/MainActivity.kt
@@ -21,7 +21,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,7 +44,13 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     //Function calls to be set here
-                    GreetingImage(message = "Happy Birthday!", from = "From")
+
+                    //avoid hard coding strings
+                    GreetingImage(
+                        message = stringResource(id = R.string.happy_birthday_text),
+                        from = stringResource(id = R.string.signature_text),
+                        modifier = Modifier
+                    )
 
                 }
             }
@@ -51,12 +59,15 @@ class MainActivity : ComponentActivity() {
 }
 
 //Use @Composable to declare compose components
+/**
+ * creates a happy birthday text
+ */
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
     //Column, Row and Box are the common ways to arrange items
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        modifier = modifier     //this modifier is passed down from function calls
     ) {
         Text(
             text = message,     //A variable can be set for the text
@@ -68,6 +79,7 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
             text = from,
             fontSize = 30.sp,
             modifier = Modifier     //another way to format text using modifiers
+                /* Always use Modifier instead of modifier */
                 .padding(16.dp)
                 .align(alignment = Alignment.End)
         )
@@ -75,25 +87,33 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
 
 }
 
+/**
+ * preview the ui in editor
+ */
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun BirthdayCardPreview(){
     BirthdayCardTheme() {
-        GreetingImage(message = "Happy Birthday!", from = "A ee e")
+        GreetingImage(message = "Happy Birthday!", from = "From: Foobar")
 
     }
 }
 
+/**
+ * displays background image and Birthday text
+ */
 @Composable
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
-    val image = painterResource(R.drawable.androidparty)
+    val image = painterResource(R.drawable.androidparty)    //image declaration
 
-    Box {
-        Image(
-            painter = image,
-            contentDescription = null
+    Box {       //box allows for element to be displayed on top of each other
+        Image(             //elements declared first will be on the back
+            painter = image,    //image is drawn
+            contentDescription = null,   //voice description for the content
+            contentScale = ContentScale.Crop,    //scales the picture to fit the whole screen
+            alpha = 0.5f    //opacity
         )
-        GreetingText(
+        GreetingText(       //other composable functions can be called
             message = message,
             from = from,
             modifier = Modifier
