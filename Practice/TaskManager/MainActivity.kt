3646144c047ef170433f3bb3cbd2a290fa4b32b7
Yuting-H <yuting.yh@gmail.com>
2023-07-11 21:40:06 -0400

    Created main in TaskManager

---
+++ Practice/TaskManager/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TaskCompleteButton(modifier = Modifier)
+                }
+            }
+        }
+    }
+}
+@Composable
+fun TaskCompleteButton(modifier: Modifier){
+    Column() {
+        val completeButton =
+        Image(painter = , contentDescription = )
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun Preview (){
+
+}
