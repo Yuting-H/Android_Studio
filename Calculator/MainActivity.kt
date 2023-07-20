22bf43245ae16f39367c0c420494e120a6f87d10
Yuting-H <yuting.yh@gmail.com>
2023-07-19 22:34:45 -0400

    Update MainActivity.kt

--- Calculator/MainActivity.kt
+++ Calculator/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +29,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun PreviewCalculator() {
+    Calculator()
+}
+
+@Composable
+fun Calculator () {
+    Column() {
+
+        Row() {
+
+        }
+
+    }
+}
+
+@Composable
+fun Button(symbol: Char, color: String,modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CalculatorTheme {
-        Greeting("Android")
-    }
+fun Circle(){
+    Canvas(modifier = Modifier.size(100.dp), onDraw = {
+        drawCircle(color = Color.Red)
+    })
 }
